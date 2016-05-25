b42e54de96220ebc4b7faba60a7f6acad8f5e0e2
raesylin <sylin_gibu@hotmail.com>
2016-05-25 15:19:24 -0700

    Fixed relative links

--- js/directives/picSheet.js
+++ js/directives/picSheet.js
@@ -3,7 +3,7 @@
 angular.module('app')
 .directive('picSheet', function() {
 	return {
-		templateUrl: '../templates/picSheet.html',
+		templateUrl: '../../templates/picSheet.html',
 		controller: 'mainCtrl',
 		replace: true
 	};
